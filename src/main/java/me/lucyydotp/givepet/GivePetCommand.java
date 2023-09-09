3d3b1849d93f7085cc5ecf6862a92d0b3d8a13b6
Lucy Poulton <lucy@lucypoulton.net>
2023-09-09 22:47:12 +0100

    rewrite time wheee

--- src/main/java/me/lucyydotp/givepet/GivePetCommand.java
+++ src/main/java/me/lucyydotp/givepet/GivePetCommand.java
@@ -3,70 +3,76 @@ package me.lucyydotp.givepet;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class GivePetCommand implements CommandExecutor, TabCompleter {
-    private final GivePet plugin;
+public class GivePetCommand implements TabExecutor {
+    private final TransferManager transferManager;
 
-    public GivePetCommand(GivePet plugin) {
-        this.plugin = plugin;
+    public GivePetCommand(TransferManager transferManager) {
+        this.transferManager = transferManager;
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("This command can only be run by a player");
+    private boolean cancel(Player player) {
+        transferManager.cancelTransfer(player);
+        return true;
+    }
+
+    private boolean accept(Player player, String[] args) {
+        if (args.length != 2) return false;
+        final var sender = Bukkit.getPlayerExact(args[1]);
+        if (sender == null) {
+            player.sendMessage(Message.FAIL_PLAYER_NOT_FOUND.text(Placeholder.unparsed("player", args[1])));
             return true;
         }
+        transferManager.acceptTransfer(sender, player);
+        return true;
+    }
+
+    private boolean deny(Player player, String[] args) {
+        if (args.length != 2) return false;
+        final var sender = Bukkit.getPlayerExact(args[1]);
+        if (sender == null) {
+            player.sendMessage(Message.FAIL_PLAYER_NOT_FOUND.text(Placeholder.unparsed("player", args[1])));
+            return true;
+        }
+        transferManager.denyTransfer(sender, player);
+        return true;
+    }
+
+    private boolean newTransfer(Player player, String[] args) {
         if (args.length != 1) return false;
-
-        if (args[0].equals("cancel")) {
-            // try to remove any outstanding attempts
-            if (plugin.cancelTransfer(player.getUniqueId())) {
-                sender.sendMessage(Message.CANCEL_SUCCESS.text());
-            } else sender.sendMessage(Message.CANCEL_FAIL.text());
+        final var recipient = Bukkit.getPlayerExact(args[0]);
+        if (recipient == null) {
+            player.sendMessage(Message.FAIL_PLAYER_NOT_FOUND.text(Placeholder.unparsed("player", args[0])));
             return true;
         }
-
-        // start a new transfer
-        Player target = Bukkit.getPlayer(args[0]);
-        if (target == null) {
-
-            sender.sendMessage(Message.FAIL_PLAYER_NOT_FOUND.text(Placeholder.unparsed("player", args[0])));
-            return true;
-        }
-        if (sender.equals(target)) {
-            sender.sendMessage(Message.FAIL_SELF.text());
-            return true;
-        }
-
-        plugin.cancelTransfer(player.getUniqueId());
-
-        // create new transfer and send message
-        sender.sendMessage(Message.START.text());
-        plugin.transferAttempts().put(player.getUniqueId(),
-                new TransferAttempt(null,
-                        player.getUniqueId(),
-                        target.getUniqueId()
-                )
-        );
+        transferManager.startTransfer(player, recipient);
         return true;
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (args.length != 1) return null;
-        List<String> outList = new ArrayList<>();
-        Bukkit.getOnlinePlayers().forEach(x -> {
-            if (!x.equals(sender)) outList.add(x.getName());
-        });
-        outList.add("cancel");
-        return outList;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("This command can only be ran by players.");
+            return true;
+        }
+
+        return switch (args[0]) {
+            case "cancel" -> cancel(player);
+            case "accept" -> accept(player, args);
+            case "deny" -> deny(player, args);
+            default -> newTransfer(player, args);
+        };
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
     }
 }
--- src/main/java/me/lucyydotp/givepet/InteractListener.java
+++ src/main/java/me/lucyydotp/givepet/InteractListener.java
@@ -1,9 +1,5 @@
 package me.lucyydotp.givepet;
 
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.AnimalTamer;
-import org.bukkit.entity.Player;
 import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,38 +20,7 @@ public class InteractListener implements Listener {
         if (!(e.getRightClicked() instanceof Tameable target)) {
             return;
         }
-        final var uuid = e.getPlayer().getUniqueId();
 
-        TransferAttempt foundAttempt = plugin.transferAttempts().get(uuid);
-        if (foundAttempt == null) return;
-
-        e.setCancelled(true);
-
-        AnimalTamer owner = target.getOwner();
-        if (owner != null && owner.getUniqueId().equals(uuid)) {
-            Player newOwner = Bukkit.getPlayer(foundAttempt.receiver());
-            if (newOwner == null) {
-                e.getPlayer().sendMessage(Message.FAIL_PLAYER_LEFT.text());
-                plugin.transferAttempts().remove(uuid);
-                return;
-            }
-            target.setOwner(newOwner);
-            target.teleport(newOwner);
-
-            newOwner.sendMessage(Message.SENT_RECEIVER.text(
-                    Placeholder.component("player", e.getPlayer().displayName()),
-                    Placeholder.component("pet", target.name())
-            ));
-
-
-            e.getPlayer().sendMessage(Message.SENT_SENDER.text(
-                    Placeholder.component("player", newOwner.displayName()),
-                    Placeholder.component("pet", target.name())
-            ));
-        } else {
-            e.getPlayer().sendMessage(Message.FAIL_NOT_OWNED.text());
-        }
-
-        plugin.transferAttempts().remove(uuid);
+        plugin.transferManager().setEntity(e.getPlayer(), target);
     }
 }
